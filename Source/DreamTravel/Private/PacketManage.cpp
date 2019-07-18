c0052e732774dfa0cef364c9af7020ced6a7e238
daitianqi <daitianqi@jd.com>
2019-07-18 11:20:01 +0800

    删除备份的数据读取

--- Source/DreamTravel/Private/PacketManage.cpp
+++ Source/DreamTravel/Private/PacketManage.cpp
@@ -122,51 +122,7 @@ int FPacketManage::ReadLastPacket()
     return 0;
 }
 
-// 数据流格式 S1 S2 24
-//int FPacketManage::ReadLastPacket(){
-//    ReadyBytesSize =  SerialClass->GetReadySize();
-//
-//    // 将数据流定向到数据包开始，不包括START_CODE_1, END_CODE
-//	if (IsLastReadPacketComplete) {
-//		// 如果上一次读取的数据包是完整的，那么上一次的数据就已经被读取了，我们这次可以重定位到START_CODE_1；
-//		// 否则，上一次的数据没被读取，我们要接着上一次的读，不能抛弃这些未读取的数据。
-//		SerialClass->ReadDataUtil(PacketBuff, START_CODE, END_CODE, ReadyBytesSize);
-//		ReadyBytesSize = SerialClass->GetReadySize();
-//	}
-//
-//	// 需要读取的数据大小，eg： 24 E S 24 E，则需要 24 E； 24 E S 24 ，则需要 24 E S 24
-//    PacketBytesNeeded = ReadyBytesSize % PACKET_SIZE + PACKET_SIZE;
-//    // 如果剩下数据少于需要的数据（即剩下的数据不足一个数据包的大小），则返回
-//    if(ReadyBytesSize < PacketBytesNeeded){
-//        UE_LOG(PacketManage, Warning, TEXT("数据不够一个数据包！ReadyBytesSize : %d, PacketBytesNeeded : %d"), ReadyBytesSize, PacketBytesNeeded);
-//		IsLastReadPacketComplete = false;
-//        return -1;
-//    }
-//	IsLastReadPacketComplete = true;
-//
-//    BytesNotNeed = ReadyBytesSize - PacketBytesNeeded;       // 不需要的数据大小
-//    //只需要后面一个完整的数据包，前面多余的数据清除，这里不用ReadDataUtil是为了优化性能
-//    while(BytesNotNeed > 0) {
-//        // 如果数据过多，超出缓存区，则考虑是开始时多余的数据，读取到最后一个buff。（只可能在最开始时发生，这些数据不做处理，直接清空）
-//        if(BytesNotNeed > PACKET_BUFF_SIZE){
-//            BytesNotNeed -= SerialClass->ReadData(PacketBuff, PACKET_BUFF_SIZE);
-//        }else{
-//            // 清除不需要的数据
-//            BytesNotNeed -= SerialClass->ReadData(PacketBuff, BytesNotNeed);
-//        }
-//    }
-//
-//    // 开始读取数据
-//	readPacketSize = SerialClass->ReadData(PacketBuff, PURE_PACKET_SIZE);
-//    // 如果读取的数据大小不等于期望的数据包大小，则数据出错，丢弃读取到的数据，不进行任何处理， 返回-1
-//    if(readPacketSize != PURE_PACKET_SIZE){
-//        UE_LOG(PacketManage, Warning, TEXT("数据包出错！ readPacketSize: %d , PacketSize-2 : %d"), readPacketSize, PURE_PACKET_SIZE);
-//
-//        return -1;
-//    }
-//
-//    return 1;
-//}
+
 // 数据包转quat
 int FPacketManage::Packet2Quat(TArray<FQuat> *PlayerBonePoses, int BoneNums, uint8 *PacketBuff)
 {
